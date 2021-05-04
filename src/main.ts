a9f5d446aa5e0e07e09d8f34bfd4f58eeebd198c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-05-04 11:36:47 -0400

    ENH: Add support for displaying notifications
    This commit adds "createNotice" to the window object and allows the display
    of status/error/warning messages while installing extensions from Slicer
    application.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import AsyncComputed from 'vue-async-computed';
+import Notifications from 'vue-notification';
 
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
@@ -7,9 +8,28 @@ import router from './router';
 
 Vue.config.productionTip = false;
 Vue.use(AsyncComputed);
+Vue.use(Notifications);
 
 new Vue({
   vuetify,
   router,
   render: (h) => h(App),
 }).$mount('#app');
+
+declare global {
+  interface Window {
+    createNotice(message: string, duration: number, type: string): void;
+  }
+}
+
+window.createNotice = (message, duration, state = 'success') => {
+  let type = state;
+  if (state === 'warning') {
+    type = 'warn';
+  }
+  Vue.notify({
+    type,
+    text: message,
+    duration,
+  });
+};
