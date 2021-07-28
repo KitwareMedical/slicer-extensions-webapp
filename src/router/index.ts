6a2a771e54661fd31caec3d34d140a625fef2cbe
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-07-28 03:25:14 -0400

    ENH: Add Home view
    Adapted from https://github.com/Joabsonlg/vuetify-landing-page
    
    See #24

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,18 @@ import Router from 'vue-router';
 
 import Catalog from '@/views/Catalog.vue';
 import ExtensionDetails from '@/views/ExtensionDetails.vue';
+import Home from '@/views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
     {
       path: '/catalog/:category/:revision/:os/:arch?',
       name: 'Catalog',
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<v-row align="center" justify="center">
+    <v-col cols="10">
+        <v-row align="center" justify="center">
+            <v-col cols="12" md="6" xl="8">
+                <h1 class="display-2 font-weight-bold mb-4">Slicer Extensions Manager</h1>
+                <h1 class="font-weight-light">
+                The Slicer community maintains this website for finding and downloading over
+                150 extensions.
+                An extension is a delivery package bundling together one or more Slicer modules.
+                After installing an extension, the associated modules will be presented to the user
+                the same way as built-in modules.
+                </h1>
+                <v-btn
+                    rounded
+                    outlined
+                    large
+                    to="/catalog"
+                    class="mt-5"
+                >
+                    Browse Extensions
+                </v-btn>
+            </v-col>
+            <v-col cols="12" md="6" xl="4" class="hidden-sm-and-down"> </v-col>
+        </v-row>
+    </v-col>
+</v-row>
+</template>
