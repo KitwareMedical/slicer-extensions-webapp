71608b7f83466d0881397c8c3bb14bdad36af521
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-08-12 16:03:57 -0400

    Fix Google Analytics integration
    This commit fixes the Google Analytics integration by ensuring the Netlify
    build time env. variable `CONTEXT` is exported into the client bundle.
    It does so by prefixing the variable with `VUE_APP_`.
    
    See https://cli.vuejs.org/guide/mode-and-env.html#environment-variables

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 process.env.VUE_APP_APP_ID = process.env.VUE_APP_APP_ID || '5f4474d0e1d8c75dfc705482';
 process.env.VUE_APP_BASE_URL = process.env.VUE_APP_BASE_URL || 'https://slicer-packages.kitware.com/api/v1';
+/* See https://docs.netlify.com/configure-builds/environment-variables/#build-metadata */
+process.env.VUE_APP_NETLIFY_CONTEXT = process.env.CONTEXT;
 
 module.exports = {
   transpileDependencies: [
