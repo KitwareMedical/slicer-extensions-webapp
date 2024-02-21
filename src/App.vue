0b5cab5049292490043110f7c228af56646064e6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-02-21 18:31:57 -0500

    feat(Home): Display "Last update" and "Revision" on regular web page footer

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,22 @@ import {
   hasExtensionManagerModel,
 } from '@/lib/api/extension.service';
 
+const BuildDate = process.env.VUE_APP_BUILD_DATE as string;
+const GitHash = process.env.VUE_APP_GIT_HASH as string;
+
 export default Vue.extend({
   computed: {
     hasExtensionManagerModel(): boolean {
       return hasExtensionManagerModel();
+    },
+    buildDate(): string {
+      return BuildDate;
+    },
+    gitRevison(): string {
+      return GitHash.slice(0, 7);
+    },
+    gitRevisionUrl(): string {
+      return `https://github.com/KitwareMedical/slicer-extensions-webapp/commit/${GitHash}`;
     },
   },
 });
@@ -28,6 +40,7 @@ export default Vue.extend({
         cols="12"
       >
         This site is maintained by <a href="https://kitware.com">@Kitware</a> on behalf of the 3D Slicer community.
+        <br>Last update {{ buildDate }}. Revision <a :href="gitRevisionUrl">{{ gitRevison }}</a>
       </v-col>
     </v-footer>
   </v-app>
